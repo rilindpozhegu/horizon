7f31b30a33f9a679550f798213cd983b7911661c
Rilind Pozhegu <Rilind Pozhegu>
2016-11-22 15:58:38 +0100

    first horizon commit

---
+++ js/main.js
@@ -0,0 +1,188 @@
+jQuery(document).ready(function($){
+	var overlayNav = $('.cd-overlay-nav'),
+		overlayContent = $('.cd-overlay-content'),
+		navigation = $('.cd-primary-nav'),
+		toggleNav = $('.cd-nav-trigger');
+
+	//inizialize navigation and content layers
+	layerInit();
+	$(window).on('resize', function(){
+		window.requestAnimationFrame(layerInit);
+	});
+
+	//open/close the menu and cover layers
+	toggleNav.on('click', function(){
+		if(!toggleNav.hasClass('close-nav')) {
+			//it means navigation is not visible yet - open it and animate navigation layer
+			toggleNav.addClass('close-nav');
+			
+			overlayNav.children('span').velocity({
+				translateZ: 0,
+				scaleX: 1,
+				scaleY: 1,
+			}, 500, 'easeInCubic', function(){
+				navigation.addClass('fade-in');
+			});
+		} else {
+			//navigation is open - close it and remove navigation layer
+			toggleNav.removeClass('close-nav');
+			
+			overlayContent.children('span').velocity({
+				translateZ: 0,
+				scaleX: 1,
+				scaleY: 1,
+			}, 500, 'easeInCubic', function(){
+				navigation.removeClass('fade-in');
+				
+				overlayNav.children('span').velocity({
+					translateZ: 0,
+					scaleX: 0,
+					scaleY: 0,
+				}, 0);
+				
+				overlayContent.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+					overlayContent.children('span').velocity({
+						translateZ: 0,
+						scaleX: 0,
+						scaleY: 0,
+					}, 0, function(){overlayContent.removeClass('is-hidden')});
+				});
+				if($('html').hasClass('no-csstransitions')) {
+					overlayContent.children('span').velocity({
+						translateZ: 0,
+						scaleX: 0,
+						scaleY: 0,
+					}, 0, function(){overlayContent.removeClass('is-hidden')});
+				}
+			});
+		}
+	});
+
+	function layerInit(){
+		var diameterValue = (Math.sqrt( Math.pow($(window).height(), 2) + Math.pow($(window).width(), 2))*2);
+		overlayNav.children('span').velocity({
+			scaleX: 0,
+			scaleY: 0,
+			translateZ: 0,
+		}, 50).velocity({
+			height : diameterValue+'px',
+			width : diameterValue+'px',
+			top : -(diameterValue/2)+'px',
+			left : -(diameterValue/2)+'px',
+		}, 0);
+
+		overlayContent.children('span').velocity({
+			scaleX: 0,
+			scaleY: 0,
+			translateZ: 0,
+		}, 50).velocity({
+			height : diameterValue+'px',
+			width : diameterValue+'px',
+			top : -(diameterValue/2)+'px',
+			left : -(diameterValue/2)+'px',
+		}, 0);
+	}
+});
+
+
+
+// Galery Filter
+/* Istope Portfolio
+-----------------------------------------------*/
+jQuery(document).ready(function($){
+
+  if ( $('.iso-box-wrapper').length > 0 ) { 
+
+      var $container  = $('.iso-box-wrapper'), 
+        $imgs     = $('.iso-box img');
+
+      $container.imagesLoaded(function () {
+
+        $container.isotope({
+        layoutMode: 'fitRows',
+        itemSelector: '.iso-box'
+        });
+
+        $imgs.load(function(){
+          $container.isotope('reLayout');
+        })
+
+      });
+
+      //filter items on button click
+
+      $('.filter-wrapper li a').click(function(){
+
+          var $this = $(this), filterValue = $this.attr('data-filter');
+
+      $container.isotope({ 
+        filter: filterValue,
+        animationOptions: { 
+            duration: 750, 
+            easing: 'linear', 
+            queue: false, 
+        }                
+      });             
+
+      // don't proceed if already selected 
+
+      if ( $this.hasClass('selected') ) { 
+        return false; 
+      }
+
+      var filter_wrapper = $this.closest('.filter-wrapper');
+      filter_wrapper.find('.selected').removeClass('selected');
+      $this.addClass('selected');
+
+        return false;
+      }); 
+
+  }
+
+});
+
+// End of galery filter
+
+// Testimonial CODE Slider + next and preview
+
+
+
+    $(document).ready(function() {
+      $("#owl-demo").owlCarousel({
+        autoPlay: 4000,
+        items : 1,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [1199,3]
+      });
+
+    });
+    
+        $(document).ready(function() {
+     
+      var owl = $("#owl-demo");
+     
+      owl.owlCarousel();
+     
+      // Custom Navigation Events
+      $(".next").click(function(){
+        owl.trigger('owl.next');
+      })
+      $(".prev").click(function(){
+        owl.trigger('owl.prev');
+      })
+     
+    });
+
+
+
+
+    $(document).ready(function() {
+      $("#owl-demo2").owlCarousel({
+        autoPlay: 3000,
+        items : 1,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [1199,3]
+      });
+
+    });
+// End Of tesimonials
